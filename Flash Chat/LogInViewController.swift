8238cb57860242e0fb955a4bbd0c17992fd3f4b2
Rodolfo Silva <rodolfoslv@gmail.com>
2019-03-07 20:58:34 -0300

    Initial commit

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,7 +6,8 @@
 
 
 import UIKit
-
+import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -25,8 +26,20 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
         
+        SVProgressHUD.show()
+        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                
+                SVProgressHUD.dismiss()
+                
+                print("Log in Successful")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         //TODO: Log in the user
         
         
